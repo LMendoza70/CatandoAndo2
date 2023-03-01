f383255ad58bbb1376e8707695ea5e73fd4a695b
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-03-01 13:56:46 -0600

    Changes to be committed: 	modified:   index.js 	new file:   src/models/user.js 	new file:   src/routes/user.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongo=require("mongoose")
+const userRoutes=require('./src/routes/user')
 
 const app=express()
 
 const port= process.env.PORT || 4000
 
 app.use(express.json())
-
-
-
+app.use('/api',userRoutes)
 
 app.get("/",(req,res)=>{
     res.send("listo")
