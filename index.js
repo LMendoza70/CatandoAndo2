e4b6ede9c38612cf30a79f380b1762c7a98d04b1
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-03-01 13:50:32 -0600

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const mongo=require("mongoose")
+
 const app=express()
 
 const port= process.env.PORT || 4000
 
-app.listen(port)
+app.use(express.json())
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("listo")
 })
 
+mongo.connect("mongodb+srv://LMendoza:luismendoza@catando.yafsmbr.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>console.log('Conectado a la BD'))
+.catch((error)=>console.error(error))
+app.listen(port)
 console.log('escuchando puesto  '+port)
